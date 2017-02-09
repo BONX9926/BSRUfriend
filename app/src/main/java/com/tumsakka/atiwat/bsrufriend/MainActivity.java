5b4d18261f5649a95b2ea402cf8909e9684d8878
winhackger <winhackger@gmail.com>
2017-02-09 15:23:50 +0700

    intent to signUp

--- app/src/main/java/com/tumsakka/atiwat/bsrufriend/MainActivity.java
+++ app/src/main/java/com/tumsakka/atiwat/bsrufriend/MainActivity.java
@@ -1,13 +1,39 @@
 package com.tumsakka.atiwat.bsrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit การประกาศตัวแปร
+    private Button sigInButton, signUpButton;
+    private EditText userEditText, passEditText;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget คือ การ Initial Var กับ view บน Xml
+        sigInButton = (Button) findViewById(R.id.button2);
+        signUpButton = (Button) findViewById(R.id.button);
+        userEditText = (EditText) findViewById(R.id.editText);
+        passEditText = (EditText) findViewById(R.id.editText2);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+    }   // Main Method
+}   // Main Class
