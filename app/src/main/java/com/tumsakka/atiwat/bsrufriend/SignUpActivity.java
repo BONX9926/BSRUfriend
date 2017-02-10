2c9c310d7f06429675f20ac8518513f223799bbb
winhackger <winhackger@gmail.com>
2017-02-10 11:33:00 +0700

    Image View OK

--- app/src/main/java/com/tumsakka/atiwat/bsrufriend/SignUpActivity.java
+++ app/src/main/java/com/tumsakka/atiwat/bsrufriend/SignUpActivity.java
@@ -1,6 +1,10 @@
 package com.tumsakka.atiwat.bsrufriend;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +21,8 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private Button button;
     private String nameString, userString, passString;
+    private Uri uri;
+    private boolean aBoolean =true;
 
 
     @Override
@@ -30,7 +36,42 @@ public class SignUpActivity extends AppCompatActivity {
 
         //Button Controller
         buttonController();
-    }
+
+        //Image Controller
+        imageController();
+    } //Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            aBoolean = false;
+            uri = data.getData();
+            //SetUP image Choose to UmageView
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+
+            }
+
+        } //if
+    }   //onActivityResult
+
+    private void imageController() {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรดเลือกแอพดูภาพ"), 1);
+
+            }   //onClick
+        });
+    }   //imageController
 
     private void buttonController() {
 
@@ -48,6 +89,13 @@ public class SignUpActivity extends AppCompatActivity {
                     //True ==> Have Space
                     MyAlert myAlert = new MyAlert(SignUpActivity.this);
                     myAlert.myDialog("มีช่องว่าง", "กรุรากรอกให้ครบทุกช่องคะ ");
+                } else if (aBoolean) {
+                    //Non Choose image
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this);
+                    myAlert.myDialog("ยังไม่เลือกรูปภาพ", "กรุณาเลือกรูปภาพ");
+                } else {
+                    //EveryThing OK
+
                 }
 
             }   //onClick
